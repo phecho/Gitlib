04ad0a040fbb6476f1cc4baec097b7d392ee2f2e
phecho <phecho@phecho.com>
2015-12-30 17:44:26 +0800

    Added extractRef support

--- src/Gitlib/Repository.php
+++ src/Gitlib/Repository.php
@@ -690,4 +690,92 @@ class Repository
 
         return $format->parse($output);
     }
+
+    public function parseCommitishPathParam($commitishPath)
+    {
+        $commitish = null;
+        $path = null;
+
+        $slashPosition = strpos($commitishPath, '/');
+        if (strlen($commitishPath) >= 40 &&
+            ($slashPosition === false ||
+             $slashPosition === 40)) {
+            // We may have a commit hash as our commitish.
+            $hash = substr($commitishPath, 0, 40);
+            if ($this->hasCommit($hash)) {
+                $commitish = $hash;
+            }
+        }
+
+        if ($commitish === null) {
+            $branches = $this->getBranches();
+
+            $tags = $this->getTags();
+            if ($tags !== null && count($tags) > 0) {
+                $branches = array_merge($branches, $tags);
+            }
+
+            $matchedBranch = null;
+            $matchedBranchLength = 0;
+            foreach ($branches as $branch) {
+                if (strpos($commitishPath, $branch) === 0 &&
+                    strlen($branch) > $matchedBranchLength) {
+                    $matchedBranch = $branch;
+                    $matchedBranchLength = strlen($matchedBranch);
+                }
+            }
+
+            if ($matchedBranch !== null) {
+                $commitish = $matchedBranch;
+            } else {
+                // We may have partial commit hash as our commitish.
+                $hash = $slashPosition === false ? $commitishPath : substr($commitishPath, 0, $slashPosition);
+                if ($this->hasCommit($hash)) {
+                    $commit = $this->getCommit($hash);
+                    $commitish = $commit->getHash();
+                } else {
+                    throw new EmptyRepositoryException('This repository is currently empty. There are no commits.');
+                }
+            }
+        }
+
+        $commitishLength = strlen($commitish);
+        $path = substr($commitishPath, $commitishLength);
+        if (strpos($path, '/') === 0) {
+            $path = substr($path, 1);
+        }
+
+        return [$commitish, $path];
+    }
+
+    public function extractRef($branch = '', $tree = '')
+    {
+        $branch = trim($branch, '/');
+        $tree = trim($tree, '/');
+        $input = $branch.'/'.$tree;
+
+        // If the ref appears to be a SHA, just split the string
+        if (preg_match('/^([[:alnum:]]{40})(.+)/', $input, $matches)) {
+            $branch = $matches[1];
+        } else {
+            // Otherwise, attempt to detect the ref using a list of the project's branches and tags
+            $validRefs = array_merge((array) $this->getBranches(), (array) $this->getTags());
+            foreach ($validRefs as $key => $ref) {
+                if (! preg_match(sprintf('#^%s/#', preg_quote($ref, '#')), $input)) {
+                    unset($validRefs[$key]);
+                }
+            }
+
+            // No exact ref match, so just try our best
+            if (count($validRefs) > 1) {
+                preg_match('/([^\/]+)(.*)/', $input, $matches);
+                $branch = preg_replace('/^\/|\/$/', '', $matches[1]);
+            } else {
+                // Extract branch name
+                $branch = array_shift($validRefs);
+            }
+        }
+
+        return [$branch, $tree];
+    }
 }
